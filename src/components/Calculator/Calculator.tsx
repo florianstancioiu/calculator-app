90cf660f82069833dcc38222a19063f303870b3d
Florian Stancioiu <florinvictorstancioiu@gmail.com>
2025-08-13 15:16:40 +0300

    Don't allow the calc to be less than 375px

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -89,7 +89,7 @@ const Calculator = () => {
   };
 
   return (
-    <div className="max-w-[20.375rem] mx-auto">
+    <div className="max-w-[20.375rem] mx-auto min-w-[20.375rem]">
       <Header />
       <Display primaryText={primaryText.trim()} secondaryText={``} />
       <Buttons onClickButton={onClickButtonHandler} values={buttons} />
